30b5882eb0e2cfd7749434ba792c9f947a55f7f4
Hans Foster <fosterhans@gmail.com>
2019-11-01 19:01:05 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,45 @@
 # code here!
+
+class School 
+  
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+    
+  end
+  
+  def add_student(student_name, student_grade)
+    if roster[student_grade] == nil
+      roster[student_grade] = []
+      roster[student_grade] << student_name
+    else 
+      roster[student_grade] << student_name
+    end
+  end
+  
+  def grade(grade_key)
+    roster[grade_key]
+  end
+  
+  def sort
+    all_values = []
+    all_keys = []
+    @roster.each_with_index do |hash, index|
+      all_values << hash[hash.keys[index]]
+      all_keys << hash.keys[index]
+    end
+    @roster.sort d
+    all_keys.sort
+    
+    hash_sorter = {}
+    
+    all_values
+  end
+  
+end
+
+  
+  
+    
